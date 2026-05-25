fe0c2ee8537eaa60135aa89c538bfe1b7f731431
DANO <dcontrerasl@live.com>
2026-05-25 17:46:29 -0400

    fix:ruta correjida a whitepaper

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -41,7 +41,7 @@ const currentYear = new Date().getFullYear();
 const primaryLinks: readonly FooterPrimaryLink[] = [
   {
     label: "White Paper",
-    href: "/public/pdf/ScorchCore_WhitePaper.pdf",
+    href: "/pdf/ScorchCore_WhitePaper.pdf",
     icon: FileText,
     disabled: true,
   },
